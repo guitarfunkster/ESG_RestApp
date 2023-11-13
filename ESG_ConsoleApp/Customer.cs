939575d1238577afb98393eb77861f7d78c11477
dstockwell <d_stockwell@ntlworld.com>
2023-11-13 22:08:49 +0000

    Extracted customer

---
+++ ESG_ConsoleApp/Customer.cs
@@ -0,0 +1,43 @@
+﻿namespace ESG_ConsoleApp
+{
+    public class Customer
+    {
+ 
+        public static string Display(Customer customer)
+        {
+            return $"Customer: ref: {customer.CustomerRef} name : {customer.CustomerName} Address Line 1:{customer.AddressLine1} ";
+        }
+
+        public Customer()
+        {
+            //Default
+        }
+
+        public Customer(int id, string customerRef, string customerName, string addressLine1, string addressLine2, string town, string county, string country, string postcode)
+        {
+            Id= id ;
+            CustomerRef = customerRef;
+            CustomerName = customerName;
+            AddressLine1 = addressLine1 ;
+            AddressLine2 = addressLine2 ;
+            Town = town ;
+            County = county ;
+            Country = country ;
+            Postcode = postcode ;
+        }
+        
+        public int Id { get; set; }
+
+        public string? CustomerRef { get; set; } 
+        public string? CustomerName { get; set; }
+        public string? AddressLine1 { get; set; } 
+        public string? AddressLine2 { get; set; }
+        public string? Town { get; set; } 
+        public string? County { get; set; } 
+        public string? Country { get; set; } 
+        public string? Postcode { get; set; }
+
+
+    }
+        
+}
--- ESG_ConsoleApp/Program.cs
+++ ESG_ConsoleApp/Program.cs
@@ -54,25 +54,10 @@ namespace ESG_ConsoleApp
 
         }
 
-        //public static string CreateDefaultFile()
-        //{
-        //    const string DefaultFileName = "Esg_TestData.csv";
-
-        //    var currentDir = Directory.GetCurrentDirectory();
-        //    Console.WriteLine($"Missing input file. Looking for default file: {DefaultFileName} in dir: {currentDir}");
-        //    var filePath = currentDir + DefaultFileName;
-        //    if (File.Exists(filePath))
-        //    {
-        //        return filePath;
-        //    }
-        //}
-
 
         public static bool ProcessInputArgs(string arg, out string filePath)
         {
            
-            //const string DefaultFileName = "Esg_TestData.csv";
-
             Console.WriteLine("Trying file argument: " + arg);
 
             //Try command line argument
@@ -121,8 +106,6 @@ namespace ESG_ConsoleApp
                // }
 
                 Console.WriteLine("Post");
-
-                // customers.ForEach(x => Console.WriteLine(x));
 
                 foreach (var customer in customers)
                 {
@@ -260,50 +243,6 @@ namespace ESG_ConsoleApp
             }
             return customer;
         }
-    }
-
-    public class Customer
-    {
-        //public override string ToString()
-        //{
-        //    return $"Customer: ref: {this.CustomerRef} name : {this.CustomerName} Address Line 1:{this.AddressLine1} ";
-        //}
-        public static string Display(Customer customer)
-        {
-            return $"Customer: ref: {customer.CustomerRef} name : {customer.CustomerName} Address Line 1:{customer.AddressLine1} ";
-        }
-
-        public Customer()
-        {
-            //Default
-        }
-
-        public Customer(int id, string customerRef, string customerName, string addressLine1, string addressLine2, string town, string county, string country, string postcode)
-        {
-            Id= id ;
-            CustomerRef = customerRef;
-            CustomerName = customerName;
-            AddressLine1 = addressLine1 ;
-            AddressLine2 = addressLine2 ;
-            Town = town ;
-            County = county ;
-            Country = country ;
-            Postcode = postcode ;
-        }
-        
-        public int Id { get; set; }
-
-        public string? CustomerRef { get; set; } 
-        public string? CustomerName { get; set; }
-        public string? AddressLine1 { get; set; } 
-        public string? AddressLine2 { get; set; }
-        public string? Town { get; set; } 
-        public string? County { get; set; } 
-        public string? Country { get; set; } 
-        public string? Postcode { get; set; }
-
-
-
     }
         
 }
